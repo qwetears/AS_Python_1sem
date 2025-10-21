4f3bf9064d9b31e39caea1717c5b14f47991a54b
qwetears <vladfedyunin6@gmail.com>
2025-10-21 11:04:54 +0300

    main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+n = int(input("Введите натуральное число n: "))
+
+sum_result = 0
+for i in range(1, n + 1):
+    sum_result += 1 / (2 ** i)
+
+print(f"Сумма ряда: {sum_result}")
