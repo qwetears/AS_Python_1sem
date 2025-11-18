da79f24422c661402b69399db7f792f1fb66dae9
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:21:52 +0300

    main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,15 @@
-if __name__ == "__main__":
-    pass
+a) try:  
+    number = float(input("Введите число: "))  # Получаем число от пользователя  
+      
+    if number < 0:  
+        raise ValueError("Число должно быть положительным")  
+    elif number == 0:  
+        raise ZeroDivisionError("Число не может быть нулевым")  
+    else:  
+        print("Число введено корректно")  
+  
+except ValueError as ve:  
+    print(f"Ошибка: {ve}")  
+except ZeroDivisionError as zde:  
+    print(f"Ошибка: {zde}")  
+b) 
