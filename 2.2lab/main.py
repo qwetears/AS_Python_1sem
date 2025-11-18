00727ca151aea4758545588c8191ac91aa67186d
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:39:27 +0300

    main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+def Mean(X, Y):    
+    if X <= 0 or Y <= 0:  
+        raise ValueError("Числа должны быть положительными")  
+      
+    AMean = (X + Y) / 2  
+        
+    import math  
+    GMean = math.sqrt(X * Y)  
+      
+    return AMean, GMean  
