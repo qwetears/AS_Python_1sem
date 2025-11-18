5918a6fa08e52440d9c0732c77d107590ba79aa0
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:23:02 +0300

    main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -12,4 +12,26 @@ except ValueError as ve:
     print(f"Ошибка: {ve}")  
 except ZeroDivisionError as zde:  
     print(f"Ошибка: {zde}")  
-b) 
+б) def check_age(age):  
+    try:  
+        age = int(age)  # Преобразуем ввод в целое число  
+          
+        if age < 18:  
+            raise ValueError("Несовершеннолетний пользователь")  
+        elif age > 100:  
+            raise ValueError("Неверный возраст")  
+        else:  
+            print("Возраст введен корректно")  
+              
+    except ValueError as e:  
+        print(f"Ошибка: {e}")  
+  
+# Пример использования:  
+user_age = input("Введите ваш возраст: ")  
+check_age(user_age)  
+в) try:  
+    number = float(input("Введите число: "))  
+    assert number >= 0, "Число должно быть положительным"  
+    print("Число введено корректно")  
+except AssertionError as e:  
+    print(e)  
