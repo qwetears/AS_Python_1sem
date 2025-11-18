a63a8a2923c5f1ff60faf6dc37fddc61007ace31
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:51:13 +0300

    main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+def copy_last_lines(input_file, output_file, K):    
+    with open(input_file, 'r', encoding='utf-8') as file:  
+        lines = file.readlines()  
+       
+    last_lines = lines[-K:]  
+       
+    with open(output_file, 'w', encoding='utf-8') as file:  
+        file.writelines(last_lines)  
+   
+input_file = 'input.txt'     
+output_file = 'output.txt'   
+K = 3                        
+  
+copy_last_lines(input_file, output_file, K)  
