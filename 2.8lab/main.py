bccca95de217db6cb747e3564656fd979f6e1934
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:52:52 +0300

    main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    pass
+def product_of_file(filename):  
+    try:  
+        with open(filename, 'r') as file:   
+            lines = file.readlines()  
+                
+            product = 1.0  
+               
+            for line in lines:  
+                try:   
+                    number = float(line.strip())   
+                    product *= number  
+                except ValueError:  
+                    print(f"Предупреждение: строка '{line.strip()}' не является числом и будет пропущена")  
+                      
+        return product  
+      
+    except FileNotFoundError:  
+        print(f"Ошибка: файл '{filename}' не найден")  
+        return None  
+    except Exception as e:  
+        print(f"Произошла ошибка: {str(e)}")  
+        return None  
