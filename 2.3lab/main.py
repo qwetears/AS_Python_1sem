92cb7d1884d512c950c9698b6906b6183b1911a2
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:41:27 +0300

    main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,18 @@
-if __name__ == "__main__":
-    pass
+def remove_row_col(A, K, L):  
+    M = len(A)        
+    N = len(A)      
+        
+    if K > M or L > N:  
+        return A  
+      
+    result =   
+      
+    for i in range(M):  
+        if i + 1 != K:  # +1 из-за индексации с 1  
+            new_row =    
+            for j in range(N):  
+                if j + 1 != L:  
+                    new_row.append(A[i][j])  
+            result.append(new_row)  
+      
+    return result  
