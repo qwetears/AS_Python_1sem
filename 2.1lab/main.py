3643e2512e7f7c272238febfa4df6db470543d33
qwetears <vladfedyunin6@gmail.com>
2025-11-18 10:35:55 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    pass
+a) def sort_dict_by_values(input_dict):  
+    sorted_asc = dict(sorted(input_dict.items(), key=lambda item: item)) [1]  
+       
+    sorted_desc = dict(sorted(input_dict.items(), key=lambda item: item, reverse=True)) [1]  
+      
+    return sorted_asc, sorted_desc  
+б) def merge_dicts(dict1, dict2):   
+    result = dict1.copy()  
+      
+    for key, value in dict2.items():  
+        if key in result:   
+            result[key] += value  
+        else:  
+            result[key] = value  
+              
+    return result  
+в) 
